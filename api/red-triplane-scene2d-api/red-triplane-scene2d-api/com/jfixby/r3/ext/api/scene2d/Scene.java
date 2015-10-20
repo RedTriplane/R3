0f328a76c1e6f6119cc9708cf0dc4aa9b0b2d450
J Fixby <github@jfixby.com>
2015-10-20 11:56:46 +0200

    ...

--- api/red-triplane-scene2d-api/red-triplane-scene2d-api/com/jfixby/r3/ext/api/scene2d/Scene.java
+++ api/red-triplane-scene2d-api/red-triplane-scene2d-api/com/jfixby/r3/ext/api/scene2d/Scene.java
@@ -4,6 +4,7 @@ import com.jfixby.cmns.api.assets.AssetID;
 import com.jfixby.cmns.api.collections.Collection;
 import com.jfixby.cmns.api.collections.Mapping;
 import com.jfixby.cmns.api.color.Color;
+import com.jfixby.r3.api.ui.unit.ComponentsFactory;
 import com.jfixby.r3.api.ui.unit.animation.Animation;
 import com.jfixby.r3.api.ui.unit.camera.Camera;
 import com.jfixby.r3.api.ui.unit.input.InputComponent;
@@ -18,7 +19,7 @@ public interface Scene extends CanvasComponent {
 	public Mapping<AssetID, InputComponent> listInputComponents();
 
 	Camera getCamera();
-	
+
 	public void setOpacity(double alpha);
 
 	public double getOpacity();
@@ -30,5 +31,9 @@ public interface Scene extends CanvasComponent {
 	public void setDebugColor(Color debug_render_color);
 
 	public Color getDebugColor();
+
+	public ComponentsFactory getComponentsFactory();
+
+	public AssetID getAssetID();
 
 }
