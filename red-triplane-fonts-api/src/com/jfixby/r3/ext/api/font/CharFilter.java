30d72bb9c78593731f58cc15e77d659a7e152be5
J Fixby <github@jfixby.com>
2015-10-26 20:31:26 +0100

    ююю

---
+++ red-triplane-fonts-api/src/com/jfixby/r3/ext/api/font/CharFilter.java
@@ -0,0 +1,7 @@
+package com.jfixby.r3.ext.api.font;
+
+public interface CharFilter {
+
+	boolean accepts(char c);
+
+}
--- red-triplane-fonts-api/src/com/jfixby/r3/ext/api/font/CharRasterInfo.java
+++ red-triplane-fonts-api/src/com/jfixby/r3/ext/api/font/CharRasterInfo.java
@@ -3,11 +3,18 @@ package com.jfixby.r3.ext.api.font;
 public class CharRasterInfo {
 
 	public String string_value;
-	public float tile_width_in_pixels;
-	public float tile_height_in_pixels;
+	public String raster_id;
+
 	public float font_size;
+
+	public float tile_width;
+	public float tile_height;
 	public float tile_origin_x;
 	public float tile_origin_y;
-	public String raster_id;
+
+	public float char_width;
+	public float char_height;
+	public float char_origin_x;
+	public float char_origin_y;
 
 }
