d27fea4c921ec081037a9ad9a20078c0a2640436
J Fixby <github@jfixby.com>
2015-10-28 19:22:32 +0100

    ...

--- red-triplane-fonts-api/src/com/jfixby/r3/ext/api/font/Font.java
+++ red-triplane-fonts-api/src/com/jfixby/r3/ext/api/font/Font.java
@@ -1,10 +1,13 @@
 package com.jfixby.r3.ext.api.font;
 
 import com.jfixby.r3.api.assets.Asset;
+import com.jfixby.r3.api.text.Chars;
 import com.jfixby.r3.api.ui.unit.txt.CharSettings;
 
 public interface Font extends Asset {
 
 	CharSettings getCharSettings(String character);
+
+	double getCharOffset(Chars chars, int char_index);
 
 }
