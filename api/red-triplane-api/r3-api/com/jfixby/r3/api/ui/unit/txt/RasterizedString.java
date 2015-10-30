1f6b848afb11deba60a5cd1cef1f5d9034c9072c
J Fixby <github@jfixby.com>
2015-10-30 14:28:40 +0100

    ...

--- api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/txt/RasterizedString.java
+++ api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/txt/RasterizedString.java
@@ -1,9 +1,9 @@
 package com.jfixby.r3.api.ui.unit.txt;
 
 import com.jfixby.cmns.api.math.Angle;
-import com.jfixby.r3.api.ui.unit.layer.Component;
+import com.jfixby.r3.api.ui.unit.layer.VisibleComponent;
 
-public interface RasterizedString extends Component {
+public interface RasterizedString extends VisibleComponent {
 
 	void setPositionXY(double canvas_x, double canvas_y);
 
