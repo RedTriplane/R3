94c410408a0c047f5f59b20e23cf99ca68b42e17
J Fixby <github@jfixby.com>
2015-11-09 18:15:58 +0100

    format

--- fokker/red-triplane-fokker-input/red-triplane-fokker-input/com/jfixby/r3/fokker/input/FokkerKeyboard.java
+++ fokker/red-triplane-fokker-input/red-triplane-fokker-input/com/jfixby/r3/fokker/input/FokkerKeyboard.java
@@ -186,913 +186,761 @@ public final class FokkerKeyboard implements Keyboard {
 
 	@Override
 	public Key NUM_1() {
-
 		return NUM_1;
 	}
 
 	@Override
 	public Key NUM_2() {
-
 		return NUM_2;
 	}
 
 	@Override
 	public Key NUM_3() {
-
 		return NUM_3;
 	}
 
 	@Override
 	public Key NUM_4() {
-
 		return NUM_4;
 	}
 
 	@Override
 	public Key NUM_5() {
-
 		return NUM_5;
 	}
 
 	@Override
 	public Key NUM_6() {
-
 		return NUM_6;
 	}
 
 	@Override
 	public Key NUM_7() {
-
 		return NUM_7;
 	}
 
 	@Override
 	public Key NUM_8() {
-
 		return NUM_8;
 	}
 
 	@Override
 	public Key NUM_9() {
-
 		return NUM_9;
 	}
 
 	@Override
 	public Key A() {
-
 		return A;
 	}
 
 	@Override
 	public Key ALT_LEFT() {
-
 		return ALT_LEFT;
 	}
 
 	@Override
 	public Key ALT_RIGHT() {
-
 		return ALT_RIGHT;
 	}
 
 	@Override
 	public Key APOSTROPHE() {
-
 		return APOSTROPHE;
 	}
 
 	@Override
 	public Key AT() {
-
 		return AT;
 	}
 
 	@Override
 	public Key B() {
-
 		return B;
 	}
 
 	@Override
 	public Key BACK() {
-
 		return BACK;
 	}
 
 	@Override
 	public Key BACKSLASH() {
-
 		return BACKSLASH;
 	}
 
 	@Override
 	public Key C() {
-
 		return C;
 	}
 
 	@Override
 	public Key CALL() {
-
 		return CALL;
 	}
 
 	@Override
 	public Key CAMERA() {
-
 		return CAMERA;
 	}
 
 	@Override
 	public Key CLEAR() {
-
 		return CLEAR;
 	}
 
 	@Override
 	public Key COMMA() {
-
 		return COMMA;
 	}
 
 	@Override
 	public Key D() {
-
 		return D;
 	}
 
 	@Override
 	public Key BACKSPACE() {
-
 		return BACKSPACE;
 	}
 
 	@Override
 	public Key FORWARD_DEL() {
-
 		return FORWARD_DEL;
 	}
 
 	@Override
 	public Key DPAD_CENTER() {
-
 		return DPAD_CENTER;
 	}
 
 	@Override
 	public Key DPAD_DOWN() {
-
 		return DPAD_DOWN;
 	}
 
 	@Override
 	public Key DPAD_LEFT() {
-
 		return DPAD_LEFT;
 	}
 
 	@Override
 	public Key DPAD_RIGHT() {
-
 		return DPAD_RIGHT;
 	}
 
 	@Override
 	public Key DPAD_UP() {
-
 		return DPAD_UP;
 	}
 
 	@Override
 	public Key CENTER() {
-
 		return CENTER;
 	}
 
 	@Override
 	public Key DOWN() {
-
 		return DOWN;
 	}
 
 	@Override
 	public Key LEFT() {
-
 		return LEFT;
 	}
 
 	@Override
 	public Key RIGHT() {
-
 		return RIGHT;
 	}
 
 	@Override
 	public Key UP() {
-
 		return UP;
 	}
 
 	@Override
 	public Key E() {
-
 		return E;
 	}
 
 	@Override
 	public Key ENDCALL() {
-
 		return ENDCALL;
 	}
 
 	@Override
 	public Key ENTER() {
-
 		return ENTER;
 	}
 
 	@Override
 	public Key ENVELOPE() {
-
 		return ENVELOPE;
 	}
 
 	@Override
 	public Key EQUALS() {
-
 		return EQUALS;
 	}
 
 	@Override
 	public Key EXPLORER() {
-
 		return EXPLORER;
 	}
 
 	@Override
 	public Key F() {
-
 		return F;
 	}
 
 	@Override
 	public Key FOCUS() {
-
 		return FOCUS;
 	}
 
 	@Override
 	public Key G() {
-
 		return G;
 	}
 
 	@Override
 	public Key GRAVE() {
-
 		return GRAVE;
 	}
 
 	@Override
 	public Key H() {
-
 		return H;
 	}
 
 	@Override
 	public Key HEADSETHOOK() {
-
 		return HEADSETHOOK;
 	}
 
 	@Override
 	public Key HOME() {
-
 		return HOME;
 	}
 
 	@Override
 	public Key I() {
-
 		return I;
 	}
 
 	@Override
 	public Key J() {
-
 		return J;
 	}
 
 	@Override
 	public Key K() {
-
 		return K;
 	}
 
 	@Override
 	public Key L() {
-
 		return L;
 	}
 
 	@Override
 	public Key LEFT_BRACKET() {
-
 		return LEFT_BRACKET;
 	}
 
 	@Override
 	public Key M() {
-
 		return M;
 	}
 
 	@Override
 	public Key MEDIA_FAST_FORWARD() {
-
 		return MEDIA_FAST_FORWARD;
 	}
 
 	@Override
 	public Key MEDIA_NEXT() {
-
 		return MEDIA_FAST_FORWARD;
 	}
 
 	@Override
 	public Key MEDIA_PLAY_PAUSE() {
-
 		return MEDIA_PLAY_PAUSE;
 	}
 
 	@Override
 	public Key MEDIA_PREVIOUS() {
-
 		return MEDIA_PREVIOUS;
 	}
 
 	@Override
 	public Key MEDIA_REWIND() {
-
 		return MEDIA_REWIND;
 	}
 
 	@Override
 	public Key MEDIA_STOP() {
-
 		return MEDIA_STOP;
 	}
 
 	@Override
 	public Key MENU() {
-
 		return MENU;
 	}
 
 	@Override
 	public Key MINUS() {
-
 		return MINUS;
 	}
 
 	@Override
 	public Key MUTE() {
-
 		return MUTE;
 	}
 
 	@Override
 	public Key N() {
-
 		return N;
 	}
 
 	@Override
 	public Key NOTIFICATION() {
-
 		return NOTIFICATION;
 	}
 
 	@Override
 	public Key NUM() {
-
 		return NUM;
 	}
 
 	@Override
 	public Key O() {
-
 		return O;
 	}
 
 	@Override
 	public Key P() {
-
 		return P;
 	}
 
 	@Override
 	public Key PERIOD() {
-
 		return PERIOD;
 	}
 
 	@Override
 	public Key PLUS() {
-
 		return PLUS;
 	}
 
 	@Override
 	public Key POUND() {
-
 		return POUND;
 	}
 
 	@Override
 	public Key POWER() {
-
 		return POWER;
 	}
 
 	@Override
 	public Key Q() {
-
 		return Q;
 	}
 
 	@Override
 	public Key R() {
-
 		return R;
 	}
 
 	@Override
 	public Key RIGHT_BRACKET() {
-
 		return RIGHT_BRACKET;
 	}
 
 	@Override
 	public Key S() {
-
 		return S;
 	}
 
 	@Override
 	public Key SEARCH() {
-
 		return SEARCH;
 	}
 
 	@Override
 	public Key SEMICOLON() {
-
 		return SEMICOLON;
 	}
 
 	@Override
 	public Key SHIFT_LEFT() {
-
 		return SHIFT_LEFT;
 	}
 
 	@Override
 	public Key SHIFT_RIGHT() {
-
 		return SHIFT_RIGHT;
 	}
 
 	@Override
 	public Key SLASH() {
-
 		return SLASH;
 	}
 
 	@Override
 	public Key SOFT_LEFT() {
-
 		return SOFT_LEFT;
 	}
 
 	@Override
 	public Key SPACE() {
-
 		return SPACE;
 	}
 
 	@Override
 	public Key SOFT_RIGHT() {
-
 		return SOFT_RIGHT;
 	}
 
 	@Override
 	public Key SYM() {
-
 		return SYM;
 	}
 
 	@Override
 	public Key T() {
-
 		return T;
 	}
 
 	@Override
 	public Key TAB() {
-
 		return TAB;
 	}
 
 	@Override
 	public Key U() {
-
 		return U;
 	}
 
 	@Override
 	public Key STAR() {
-
 		return STAR;
 	}
 
 	@Override
 	public Key UNKNOWN() {
-
 		return UNKNOWN;
 	}
 
 	@Override
 	public Key V() {
-
 		return V;
 	}
 
 	@Override
 	public Key VOLUME_DOWN() {
-
 		return VOLUME_DOWN;
 	}
 
 	@Override
 	public Key VOLUME_UP() {
-
 		return VOLUME_UP;
 	}
 
 	@Override
 	public Key W() {
-
 		return W;
 	}
 
 	@Override
 	public Key X() {
-
 		return X;
 	}
 
 	@Override
 	public Key Y() {
-
 		return Y;
 	}
 
 	@Override
 	public Key Z() {
-
 		return Z;
 	}
 
 	@Override
 	public Key META_ALT_LEFT_ON() {
-
 		return META_ALT_LEFT_ON;
 	}
 
 	@Override
 	public Key META_ALT_ON() {
-
 		return META_ALT_ON;
 	}
 
 	@Override
 	public Key META_ALT_RIGHT_ON() {
-
 		return META_ALT_RIGHT_ON;
 	}
 
 	@Override
 	public Key META_SHIFT_LEFT_ON() {
-
 		return META_SHIFT_LEFT_ON;
 	}
 
 	@Override
 	public Key META_SHIFT_ON() {
-
 		return META_SHIFT_ON;
 	}
 
 	@Override
 	public Key META_SHIFT_RIGHT_ON() {
-
 		return META_SHIFT_RIGHT_ON;
 	}
 
 	@Override
 	public Key META_SYM_ON() {
-
 		return META_SYM_ON;
 	}
 
 	@Override
 	public Key CONTROL_LEFT() {
-
 		return CONTROL_LEFT;
 	}
 
 	@Override
 	public Key CONTROL_RIGHT() {
-
 		return CONTROL_RIGHT;
 	}
 
 	@Override
 	public Key ESCAPE() {
-
 		return ESCAPE;
 	}
 
 	@Override
 	public Key END() {
-
 		return END;
 	}
 
 	@Override
 	public Key INSERT() {
-
 		return INSERT;
 	}
 
 	@Override
 	public Key PAGE_UP() {
-
 		return PAGE_UP;
 	}
 
 	@Override
 	public Key PAGE_DOWN() {
-
 		return PAGE_DOWN;
 	}
 
 	@Override
 	public Key PICTSYMBOLS() {
-
 		return PICTSYMBOLS;
 	}
 
 	@Override
 	public Key SWITCH_CHARSET() {
-
 		return SWITCH_CHARSET;
 	}
 
 	@Override
 	public Key BUTTON_CIRCLE() {
-
 		return BUTTON_CIRCLE;
 	}
 
 	@Override
 	public Key BUTTON_A() {
-
 		return BUTTON_A;
 	}
 
 	@Override
 	public Key BUTTON_B() {
-
 		return BUTTON_B;
 	}
 
 	@Override
 	public Key BUTTON_C() {
-
 		return BUTTON_C;
 	}
 
 	@Override
 	public Key BUTTON_X() {
-
 		return BUTTON_X;
 	}
 
 	@Override
 	public Key BUTTON_Y() {
-
 		return BUTTON_Y;
 	}
 
 	@Override
 	public Key BUTTON_Z() {
-
 		return BUTTON_Z;
 	}
 
 	@Override
 	public Key BUTTON_L1() {
-
 		return BUTTON_L1;
 	}
 
 	@Override
 	public Key BUTTON_R1() {
-
 		return BUTTON_R1;
 	}
 
 	@Override
 	public Key BUTTON_R2() {
-
 		return BUTTON_R2;
 	}
 
 	@Override
 	public Key BUTTON_L2() {
-
 		return BUTTON_L2;
 	}
 
 	@Override
 	public Key BUTTON_THUMBL() {
-
 		return BUTTON_THUMBL;
 	}
 
 	@Override
 	public Key BUTTON_START() {
-
 		return BUTTON_START;
 	}
 
 	@Override
 	public Key BUTTON_MODE() {
-
 		return BUTTON_MODE;
 	}
 
 	@Override
 	public Key BUTTON_SELECT() {
-
 		return BUTTON_SELECT;
 	}
 
 	@Override
 	public Key BUTTON_THUMBR() {
-
 		return BUTTON_THUMBR;
 	}
 
 	@Override
 	public Key NUMPAD_0() {
-
 		return NUMPAD_0;
 	}
 
 	@Override
 	public Key NUMPAD_1() {
-
 		return NUMPAD_1;
 	}
 
 	@Override
 	public Key NUMPAD_2() {
-
 		return NUMPAD_2;
 	}
 
 	@Override
 	public Key NUMPAD_3() {
-
 		return NUMPAD_3;
 	}
 
 	@Override
 	public Key NUMPAD_4() {
-
 		return NUMPAD_4;
 	}
 
 	@Override
 	public Key NUMPAD_5() {
-
 		return NUMPAD_5;
 	}
 
 	@Override
 	public Key NUMPAD_6() {
-
 		return NUMPAD_6;
 	}
 
 	@Override
 	public Key NUMPAD_7() {
-
 		return NUMPAD_7;
 	}
 
 	@Override
 	public Key NUMPAD_8() {
-
 		return NUMPAD_8;
 	}
 
 	@Override
 	public Key NUMPAD_9() {
-
 		return NUMPAD_9;
 	}
 
 	@Override
 	public Key COLON() {
-
 		return COLON;
 	}
 
 	@Override
 	public Key F1() {
-
 		return F1;
 	}
 
 	@Override
 	public Key F2() {
-
 		return F2;
 	}
 
 	@Override
 	public Key F3() {
-
 		return F3;
 	}
 
 	@Override
 	public Key F4() {
-
 		return F4;
 	}
 
 	@Override
 	public Key F5() {
-
 		return F5;
 	}
 
 	@Override
 	public Key F6() {
-
 		return F6;
 	}
 
 	@Override
 	public Key F7() {
-
 		return F7;
 	}
 
 	@Override
 	public Key F8() {
-
 		return F8;
 	}
 
 	@Override
 	public Key F9() {
-
 		return F9;
 	}
 
 	@Override
 	public Key F10() {
-
 		return F10;
 	}
 
 	@Override
 	public Key F11() {
-
 		return F11;
 	}
 
 	@Override
 	public Key F12() {
-
 		return F12;
 	}
 
@@ -1100,5 +948,4 @@ public final class FokkerKeyboard implements Keyboard {
 	public KeysList listAllKeys() {
 		return this.KEYS_LIST;
 	}
-
 }
