2a2e983fe8c8c024eb5fc2bb17a64f389d47f0a7
J Fixby <github@jfixby.com>
2015-11-16 15:59:46 +0100

    move system variables RedTriplane -> Sys

--- api/red-triplane-api/r3-api/com/jfixby/r3/api/ExecutionMode.java
+++
@@ -1,7 +0,0 @@
-package com.jfixby.r3.api;
-
-public enum ExecutionMode {
-
-	EARLY_DEVELOPMENT, PUBLIC_RELEASE;
-
-}
--- api/red-triplane-api/r3-api/com/jfixby/r3/api/RedTriplane.java
+++ api/red-triplane-api/r3-api/com/jfixby/r3/api/RedTriplane.java
@@ -20,40 +20,7 @@ public class RedTriplane {
 		return componentInstaller.getComponent();
 	}
 
-	public static void setExecutionMode(ExecutionMode developer) {
-		componentInstaller.invokeComponent().setExecutionMode(developer);
-	}
-
-	public static void setFlag(String flag_name, boolean flag_value) {
-		componentInstaller.invokeComponent().setFlag(flag_name,
-				flag_value);
-	}
-
-	public static boolean getFlag(String flag_name) {
-		return componentInstaller.invokeComponent().getFlag(flag_name);
-	}
-
-	public static String getStringParameter(String parameter_name) {
-		return componentInstaller.invokeComponent().getStringParameter(
-				parameter_name);
-	}
-
-	public static void setStringParameter(String parameter_name,
-			String parameter_value) {
-		componentInstaller.invokeComponent().setStringParameter(parameter_name,
-				parameter_value);
-	}
-
-	public static void setSystemAssetID(String parameter_name,
-			AssetID parameter_value) {
-		componentInstaller.invokeComponent().setSystemAssetID(parameter_name,
-				parameter_value);
-	}
-
-	public static AssetID getSystemAssetID(String parameter_name) {
-		return componentInstaller.invokeComponent().getSystemAssetID(
-				parameter_name);
-	}
+	
 
 
 }
