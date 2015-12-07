c5f8f87b2748ff960c6de8f40fae476e9bec6964
J Fixby <github@jfixby.com>
2015-12-07 21:20:48 +0100

    ...

--- api/red-triplane-api/r3-api/com/jfixby/r3/api/RedTriplane.java
+++ api/red-triplane-api/r3-api/com/jfixby/r3/api/RedTriplane.java
@@ -1,6 +1,5 @@
 package com.jfixby.r3.api;
 
-import com.jfixby.cmns.api.assets.AssetID;
 import com.jfixby.cmns.api.components.ComponentInstaller;
 
 public class RedTriplane {
--- api/red-triplane-api/r3-api/com/jfixby/r3/api/RedTriplaneComponent.java
+++ api/red-triplane-api/r3-api/com/jfixby/r3/api/RedTriplaneComponent.java
@@ -1,6 +1,5 @@
 package com.jfixby.r3.api;
 
-import com.jfixby.cmns.api.assets.AssetID;
 
 public interface RedTriplaneComponent {
 
