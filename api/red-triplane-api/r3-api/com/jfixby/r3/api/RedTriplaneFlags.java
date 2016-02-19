06c1441185753a36114826e71e648741a9e1172c
J Fixby <github@jfixby.com>
2016-02-19 23:44:14 +0100

    +SandBoxFlag

--- api/red-triplane-api/r3-api/com/jfixby/r3/api/RedTriplaneFlags.java
+++ api/red-triplane-api/r3-api/com/jfixby/r3/api/RedTriplaneFlags.java
@@ -5,6 +5,7 @@ public class RedTriplaneFlags {
 	public static final String PrintLogMessageOnMissingSprite = "PrintLogMessageOnMissingSprite";
 	public static final String ExitOnMissingSprite = "ExitOnMissingSprite";
 	public static final String AllowMissingRaster = "AllowMissingRaster";
+	
 
 	
 
