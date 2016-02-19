c66dc607150672c34115b57aaf2269c33dd521e4
J Fixby <github@jfixby.com>
2016-02-19 12:53:29 +0100

    +shaders

---
+++ api/red-triplane-scene2d-api/red-triplane-scene2d-api/com/jfixby/r3/ext/api/scene2d/srlz/ShaderParameter.java
@@ -0,0 +1,16 @@
+package com.jfixby.r3.ext.api.scene2d.srlz;
+
+public class ShaderParameter {
+
+	public ShaderParameter(String name, String value) {
+		this.name = name;
+		this.value = value;
+	}
+
+	public ShaderParameter() {
+	}
+
+	public String name;
+	public String value;
+
+}
