941bd7e48edc9545815f015b1a6d4a59e314fb2c
J Fixby <github@jfixby.com>
2016-02-22 20:42:48 +0100

    ...

--- fokker/red-triplane-shader-fokker/src/com/jfixby/r3/shader/fokker/ShaderEntry.java
+++ fokker/red-triplane-shader-fokker/src/com/jfixby/r3/shader/fokker/ShaderEntry.java
@@ -2,11 +2,12 @@ package com.jfixby.r3.shader.fokker;
 
 import com.jfixby.cmns.api.assets.AssetID;
 import com.jfixby.cmns.api.file.File;
+import com.jfixby.r3.api.shader.ShaderAsset;
 import com.jfixby.r3.api.shader.ShaderInfo;
 import com.jfixby.r3.api.shader.ShadersContainer;
 import com.jfixby.rana.api.asset.Asset;
 
-public class ShaderEntry implements Asset {
+public class ShaderEntry implements Asset, ShaderAsset {
 	AssetID asset_id;
 	ShaderInfo shader;
 	File shader_folder;
