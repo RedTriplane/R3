77d7428096c73f1f88ef73c39118077688985941
J Fixby <github@jfixby.com>
2016-03-08 15:34:00 +0100

    ...

--- fokker/red-triplane-fokker-assembler/red-triplane-fokker-assembler/com/jfixby/red/triplane/fokker/depsearch/ScanClassPathCollectJars.java
+++ fokker/red-triplane-fokker-assembler/red-triplane-fokker-assembler/com/jfixby/red/triplane/fokker/depsearch/ScanClassPathCollectJars.java
@@ -2,9 +2,11 @@ package com.jfixby.red.triplane.fokker.depsearch;
 
 import java.io.IOException;
 
+import com.jfixby.cmns.api.collections.Collections;
 import com.jfixby.cmns.api.collections.List;
 import com.jfixby.cmns.api.file.ChildrenList;
 import com.jfixby.cmns.api.file.File;
+import com.jfixby.cmns.api.file.FileFilter;
 import com.jfixby.cmns.api.file.LocalFileSystem;
 import com.jfixby.cmns.api.log.L;
 import com.jfixby.cmns.api.util.JUtils;
@@ -18,18 +20,25 @@ public class ScanClassPathCollectJars {
 		File gradle_path = LocalFileSystem.newFile(gradle_path_string);
 		File jars = LocalFileSystem.ApplicationHome().child("jars");
 		jars.clearFolder();
-		ChildrenList folders = gradle_path.listChildren().filter(file -> file.isFolder());
+		FileFilter filter = file -> file.isFolder();
+		ChildrenList folders = gradle_path.listChildren().filter(filter);
+		File core = folders.findChild("core");
+		List<String> core_jars = collectJars(core, jars, Collections.newList());
+		core_jars.print("core_jars");
 		for (int i = 0; i < folders.size(); i++) {
 			File subfolder = folders.getElementAt(i);
-			collectJars(subfolder, jars);
+			if (subfolder == core) {
+				continue;
+			}
+			collectJars(subfolder, jars, core_jars);
 		}
 
 	}
 
-	private static void collectJars(File subfolder, File jars) throws IOException {
+	private static List<String> collectJars(File subfolder, File jars, List<String> ignore_jars) throws IOException {
 		File class_path_file = subfolder.listChildren().findChild(".classpath");
 		if (class_path_file == null) {
-			return;
+			return null;
 		}
 		String child_name = subfolder.getName();
 		File jar_child_folder = jars.child(child_name);
@@ -43,14 +52,21 @@ public class ScanClassPathCollectJars {
 
 		L.d(subfolder);
 
+		List<String> processed_jars = Collections.newList();
+
 		for (int i = 0; i < deps_list.size(); i++) {
 			String element = deps_list.getElementAt(i);
 			String jar_path = getJarPath(element);
+
 			if (jar_path == null) {
+				continue;
+			}
+			if (ignore_jars.contains(jar_path)) {
 				continue;
 			}
 			L.d();
 			L.d("jar_path", jar_path);
+			processed_jars.add(jar_path);
 
 			File jar = LocalFileSystem.newFile(jar_path);
 			LocalFileSystem.copyFileToFolder(jar, jar_child_folder);
@@ -62,6 +78,7 @@ public class ScanClassPathCollectJars {
 				LocalFileSystem.copyFileToFolder(src, jar_child_folder.child("src"));
 			}
 		}
+		return processed_jars;
 
 	}
 
