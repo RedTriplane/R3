76122aa600178b370f18373c1e87f3124f715f81
J Fixby <github@jfixby.com>
2016-03-11 15:20:56 +0100

    +	"libGDX.Atlas.ETC1Compressed";

--- api/red-triplane-api/r3-api/com/jfixby/r3/api/resources/StandardPackageFormats.java
+++ api/red-triplane-api/r3-api/com/jfixby/r3/api/resources/StandardPackageFormats.java
@@ -1,24 +1,25 @@
 package com.jfixby.r3.api.resources;
 
 public class StandardPackageFormats {
-	public static final class Adobe {
-		public static final String PSD = "Photoshop.PSD";
-	}
+    public static final class Adobe {
+	public static final String PSD = "Photoshop.PSD";
+    }
 
-	public static final class libGDX {
-		public static final String Atlas = "libGDX.Atlas";
-		public static final String Texture = "libGDX.Texture";
-		public static final String Atlas_GWT = "libGDX.Atlas.Gwt";
-		public static final String Texture_GWT = "libGDX.Texture.Gwt";
-	}
+    public static final class libGDX {
+	public static final String Atlas = "libGDX.Atlas";
+	public static final String Atlas_ETC1Compressed = "libGDX.Atlas.ETC1Compressed";
+	public static final String Texture = "libGDX.Texture";
+	public static final String Atlas_GWT = "libGDX.Atlas.Gwt";
+	public static final String Texture_GWT = "libGDX.Texture.Gwt";
+    }
 
-	public static final class RedTriplane {
-		public static final String TiledRaster = "RedTriplane.TiledRaster";
-		public static final String Shader = "RedTriplane.Shader";
-		public static final String Text = "RedTriplane.Text";
-	}
+    public static final class RedTriplane {
+	public static final String TiledRaster = "RedTriplane.TiledRaster";
+	public static final String Shader = "RedTriplane.Shader";
+	public static final String Text = "RedTriplane.Text";
+    }
 
-	public static final class Box2DEditor {
-		public static final String Project = "Box2DEditor.Project";
-	}
+    public static final class Box2DEditor {
+	public static final String Project = "Box2DEditor.Project";
+    }
 }
