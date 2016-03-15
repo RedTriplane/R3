7e7927f89f8e8d57977e3b2b4d1f8d6bc430c52d
J Fixby <github@jfixby.com>
2016-03-15 14:58:38 +0100

    +print system settings

--- api/red-triplane-api/r3-api/com/jfixby/r3/api/RedTriplaneParams.java
+++ api/red-triplane-api/r3-api/com/jfixby/r3/api/RedTriplaneParams.java
@@ -2,9 +2,10 @@ package com.jfixby.r3.api;
 
 public class RedTriplaneParams {
 
-	public static final String RASTER_IS_MISING = "RASTER_IS_MISING";
-	public static final String SPRITE_BLACK = "BLACK";
-	public static final String SPRITE_BLACK_DEBUG = "BLACK_DEBUG";
-	public static final String DefaultFont = "DefaultFont";
+    public static final String RASTER_IS_MISING = "RASTER_IS_MISING";
+    public static final String SPRITE_BLACK = "BLACK";
+    public static final String SPRITE_BLACK_DEBUG = "BLACK_DEBUG";
+    public static final String DefaultFont = "DefaultFont";
+    public static final String CLEAR_SCREEN_COLOR_ARGB = "CLEAR_SCREEN_COLOR_ARGB";
 
 }
