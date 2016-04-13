8d23f71a829aedb01e1128d1e73092e91b6e0abb
J Fixby <github@jfixby.com>
2016-04-13 14:19:04 +0200

    code format

--- api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/GameUI.java
+++ api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/GameUI.java
@@ -1,3 +1,4 @@
+
 package com.jfixby.r3.api.ui;
 
 import com.jfixby.cmns.api.ComponentInstaller;
@@ -7,59 +8,58 @@ import com.jfixby.r3.api.game.LoadTask;
 
 public class GameUI {
 
-    static private ComponentInstaller<GameUIComponent> componentInstaller = new ComponentInstaller<GameUIComponent>(
-	    "GameUI");
+	static private ComponentInstaller<GameUIComponent> componentInstaller = new ComponentInstaller<GameUIComponent>("GameUI");
 
-    public static final void installComponent(GameUIComponent component_to_install) {
-	componentInstaller.installComponent(component_to_install);
-    }
+	public static final void installComponent (final GameUIComponent component_to_install) {
+		componentInstaller.installComponent(component_to_install);
+	}
 
-    public static final GameUIComponent invoke() {
-	return componentInstaller.invokeComponent();
-    }
+	public static final GameUIComponent invoke () {
+		return componentInstaller.invokeComponent();
+	}
 
-    public static final GameUIComponent component() {
-	return componentInstaller.getComponent();
-    }
+	public static final GameUIComponent component () {
+		return componentInstaller.getComponent();
+	}
 
-    public static void showLoadingScreen(AssetID loader_unit_id, boolean fadedOut) {
-	invoke().showLoadingScreen(loader_unit_id, fadedOut);
-    }
+	public static void showLoadingScreen (final AssetID loader_unit_id, final boolean fadedOut) {
+		invoke().showLoadingScreen(loader_unit_id, fadedOut);
+	}
 
-    public static LoadTask prepareLoadGameUITask(Collection<AssetID> asetsToLoad) {
-	return invoke().prepareLoadGameUITask(asetsToLoad);
-    }
+	public static LoadTask prepareLoadGameUITask (final Collection<AssetID> asetsToLoad) {
+		return invoke().prepareLoadGameUITask(asetsToLoad);
+	}
 
-    public static LoadTask prepareLoadGameUITask(AssetID... asetsToLoad) {
-	return invoke().prepareLoadGameUITask(asetsToLoad);
-    }
+	public static LoadTask prepareLoadGameUITask (final AssetID... asetsToLoad) {
+		return invoke().prepareLoadGameUITask(asetsToLoad);
+	}
 
-    public static void pushTaskToLoader(LoadTask task, UILoaderListener ui_loader_listener) {
-	invoke().pushTaskToLoader(task, ui_loader_listener);
-    }
+	public static void pushTaskToLoader (final LoadTask task, final UILoaderListener ui_loader_listener) {
+		invoke().pushTaskToLoader(task, ui_loader_listener);
+	}
 
-    public static void pushFadeOut(long period) {
-	invoke().pushFadeOut(period);
-    }
+	public static void pushFadeOut (final long period) {
+		invoke().pushFadeOut(period);
+	}
 
-    public static void switchToGameUI(AssetID game_ui_unit_id) {
-	invoke().switchToGameUI(game_ui_unit_id);
-    }
+	public static void switchToGameUI (final AssetID game_ui_unit_id) {
+		invoke().switchToGameUI(game_ui_unit_id);
+	}
 
-    public static void pushFadeIn(long period) {
-	invoke().pushFadeIn(period);
-    }
+	public static void pushFadeIn (final long period) {
+		invoke().pushFadeIn(period);
+	}
 
-    public static void allowUserInput() {
-	invoke().allowUserInput();
-    }
+	public static void allowUserInput () {
+		invoke().allowUserInput();
+	}
 
-    public static void pushLoadAssetsTask(Collection<AssetID> newList, UILoaderListener loader_listener) {
-	invoke().pushLoadAssetsTask(newList, loader_listener);
-    }
+	public static void pushLoadAssetsTask (final Collection<AssetID> newList, final UILoaderListener loader_listener) {
+		invoke().pushLoadAssetsTask(newList, loader_listener);
+	}
 
-    public static AnimationsMachine newAnimationsMachine() {
-	return invoke().newAnimationsMachine();
-    }
+	public static AnimationsMachine newAnimationsMachine () {
+		return invoke().newAnimationsMachine();
+	}
 
 }
