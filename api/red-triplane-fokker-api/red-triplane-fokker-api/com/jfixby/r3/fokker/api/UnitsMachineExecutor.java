92398161114dd632311a4f94dc4475d29435f9ae
J Fixby <github@jfixby.com>
2016-04-13 20:37:17 +0200

    ...

--- api/red-triplane-fokker-api/red-triplane-fokker-api/com/jfixby/r3/fokker/api/UnitsMachineExecutor.java
+++ api/red-triplane-fokker-api/red-triplane-fokker-api/com/jfixby/r3/fokker/api/UnitsMachineExecutor.java
@@ -1,18 +1,18 @@
+
 package com.jfixby.r3.fokker.api;
 
 public interface UnitsMachineExecutor {
 
-	public void doDeploy();
+	public void doDeploy ();
 
-	public void doUpdate(EngineState engine_state);
-	public void doRender(EngineState engine_state);
+	public void doUpdate (EngineState engine_state);
 
-	public void doDispose();
+	public void doRender (EngineState engine_state);
 
-	public void doPause();
+	public void doDispose ();
 
-	public void doResume();
+	public void doPause ();
 
-	
+	public void doResume ();
 
 }
