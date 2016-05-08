2110ed86b83e4a14928c4f4d2dab3cba11fa3c10
J Fixby <github@jfixby.com>
2016-05-08 11:07:42 +0200

    ...

--- api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/txt/CharSettings.java
+++
@@ -1,9 +0,0 @@
-package com.jfixby.r3.api.ui.unit.txt;
-
-import com.jfixby.cmns.api.assets.AssetID;
-
-public interface CharSettings {
-
-	AssetID getRasterID();
-
-}
--- api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/txt/TextFactory.java
+++ api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/txt/TextFactory.java
@@ -1,27 +1,24 @@
+
 package com.jfixby.r3.api.ui.unit.txt;
 
-import com.jfixby.r3.api.ui.unit.raster.CharRaster;
-
 public interface TextFactory {
-	
+
 	public static final String Pangram_EN = "The quick brown fox jumps over the lazy dog.";
 	public static final String Pangram_RU = "Съешь же ещё этих мягких французских булок, да выпей чаю.";
 	public static final String Pangram_IT = "Quel vituperabile xenofobo zelante assaggia il whisky ed esclama: alleluja!";
 
-	TextBarSpecs newTextBarSpecs();
+	TextBarSpecs newTextBarSpecs ();
 
-	TextBar newTextBar(TextBarSpecs text_specs);
+	TextBar newTextBar (TextBarSpecs text_specs);
 
-	FontSize newFontSize();
+	FontSize newFontSize ();
 
-	CharRaster newCharTile(CharSettings char_settings);
+	RasterizedStringSpecs newRasterStringSpecs ();
 
-	RasterizedStringSpecs newRasterStringSpecs();
+	RasterizedString newRasterString (RasterizedStringSpecs specs);
 
-	RasterizedString newRasterString(RasterizedStringSpecs specs);
+	RasterizedFont newFont (RasterizedFontSpecs specs);
 
-	RasterizedFont newFont(RasterizedFontSpecs specs);
-
-	RasterizedFontSpecs newFontSpecs();
+	RasterizedFontSpecs newFontSpecs ();
 
 }
