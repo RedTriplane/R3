8644813f34a2deb441e2408c77ff9620d6e23560
J Fixby <github@jfixby.com>
2016-05-10 22:37:18 +0200

    ...

--- api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/input/CustomInput.java
+++ api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/input/CustomInput.java
@@ -3,6 +3,7 @@ package com.jfixby.r3.api.ui.unit.input;
 
 import com.jfixby.cmns.api.collections.Collection;
 import com.jfixby.cmns.api.floatn.Float2;
+import com.jfixby.cmns.api.geometry.CanvasPosition;
 import com.jfixby.r3.api.ui.unit.layer.VisibleComponent;
 import com.jfixby.r3.api.ui.unit.raster.Raster;
 
@@ -16,7 +17,7 @@ public interface CustomInput extends VisibleComponent, InputComponent {
 
 	void setPosition (Float2 position);
 
-	Float2 getPosition ();
+	CanvasPosition getPosition ();
 
 	void updateChildrenPositionRespectively ();
 
