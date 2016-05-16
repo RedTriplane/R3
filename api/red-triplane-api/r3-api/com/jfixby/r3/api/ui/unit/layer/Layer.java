33fdd9557b778c82aca54b03c52f2744ab9bb4d4
J Fixby <github@jfixby.com>
2016-05-16 20:50:03 +0200

    +find component by Name

--- api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/layer/Layer.java
+++ api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/layer/Layer.java
@@ -1,6 +1,7 @@
 
 package com.jfixby.r3.api.ui.unit.layer;
 
+import com.jfixby.cmns.api.assets.AssetID;
 import com.jfixby.cmns.api.collections.Collection;
 import com.jfixby.r3.api.ui.unit.ComponentsFactory;
 import com.jfixby.r3.api.ui.unit.camera.Camera;
@@ -39,6 +40,10 @@ public interface Layer extends VisibleComponent {
 	public <Q extends NamedComponent> Collection<Q> findComponents (String element_name);
 
 	public <Q extends NamedComponent> Q findComponent (String element_name);
+
+	public <Q extends NamedComponent> Collection<Q> findComponents (AssetID element_name);
+
+	public <Q extends NamedComponent> Q findComponent (AssetID element_name);
 
 	public <Q extends Component> Q findComponent ();
 
