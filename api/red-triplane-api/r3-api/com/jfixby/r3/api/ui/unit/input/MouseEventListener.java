f479c09b96d40ea6b4d19b1c835ef3c14355be3c
J Fixby <github@jfixby.com>
2016-05-17 11:01:18 +0200

    input fix

--- api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/input/MouseEventListener.java
+++ api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/input/MouseEventListener.java
@@ -1,3 +1,4 @@
+
 package com.jfixby.r3.api.ui.unit.input;
 
 import com.jfixby.cmns.api.log.L;
@@ -8,37 +9,37 @@ public interface MouseEventListener extends Component {
 	public static final MouseEventListener DEBUG = new MouseEventListener() {
 
 		@Override
-		public boolean onMouseMoved(MouseMovedEvent event) {
-			L.d(event);
+		public boolean onMouseMoved (final MouseMovedEvent event) {
+			L.d("DEBUG", event);
 			return true;
 		}
 
 		@Override
-		public boolean onTouchDown(TouchDownEvent event) {
-			L.d(event);
+		public boolean onTouchDown (final TouchDownEvent event) {
+			L.d("DEBUG", event);
 			return true;
 		}
 
 		@Override
-		public boolean onTouchUp(TouchUpEvent event) {
-			L.d(event);
+		public boolean onTouchUp (final TouchUpEvent event) {
+			L.d("DEBUG", event);
 			return true;
 		}
 
 		@Override
-		public boolean onTouchDragged(TouchDraggedEvent event) {
-			L.d(event);
+		public boolean onTouchDragged (final TouchDraggedEvent event) {
+			L.d("DEBUG", event);
 			return true;
 		}
 
 	};
 
-	boolean onMouseMoved(MouseMovedEvent input_event);
+	boolean onMouseMoved (MouseMovedEvent input_event);
 
-	boolean onTouchDown(TouchDownEvent input_event);
+	boolean onTouchDown (TouchDownEvent input_event);
 
-	boolean onTouchUp(TouchUpEvent input_event);
+	boolean onTouchUp (TouchUpEvent input_event);
 
-	boolean onTouchDragged(TouchDraggedEvent input_event);
+	boolean onTouchDragged (TouchDraggedEvent input_event);
 
 }
