ab803322b3fb36f143c49f16e99f8eaf30104af6
J Fixby <github@jfixby.com>
2016-06-03 02:53:35 +0200

    +RasterPool

--- api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/raster/RasterComponentsFactory.java
+++ api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/raster/RasterComponentsFactory.java
@@ -1,11 +1,14 @@
+
 package com.jfixby.r3.api.ui.unit.raster;
 
 import com.jfixby.cmns.api.assets.AssetID;
 
 public interface RasterComponentsFactory {
 
-	Tile newTile(AssetID asset_id);
+	Tile newTile (AssetID asset_id);
 
-	Raster newRaster(AssetID asset_id);
+	Raster newRaster (AssetID asset_id);
+
+	RasterPool newRasterPool (AssetID assetID);
 
 }
---
+++ api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/raster/RasterPool.java
@@ -0,0 +1,14 @@
+
+package com.jfixby.r3.api.ui.unit.raster;
+
+import com.jfixby.cmns.api.assets.AssetID;
+import com.jfixby.r3.api.ui.unit.ComponentsFactory;
+import com.jfixby.r3.api.ui.unit.layer.VisibleComponent;
+
+public interface RasterPool extends VisibleComponent {
+
+	public AssetID getAssetID ();
+
+	public ComponentsFactory getComponentsFactory ();
+
+}
