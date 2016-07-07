034cedcd5d670d58a9175bf2db1190639a98f7a6
J Fixby <github@jfixby.com>
2016-07-07 12:56:32 +0200

    +scene disposal?

--- api/red-triplane-scene2d-api/red-triplane-scene2d-api/com/jfixby/r3/ext/api/scene2d/Scene2D.java
+++ api/red-triplane-scene2d-api/red-triplane-scene2d-api/com/jfixby/r3/ext/api/scene2d/Scene2D.java
@@ -1,3 +1,4 @@
+
 package com.jfixby.r3.ext.api.scene2d;
 
 import com.jfixby.cmns.api.ComponentInstaller;
@@ -6,43 +7,44 @@ import com.jfixby.rana.api.pkg.PackageReader;
 
 public class Scene2D {
 
-	static private ComponentInstaller<Scene2DComponent> componentInstaller = new ComponentInstaller<Scene2DComponent>(
-			"Scene2D");
+	static private ComponentInstaller<Scene2DComponent> componentInstaller = new ComponentInstaller<Scene2DComponent>("Scene2D");
 
-	public static final void installComponent(
-			Scene2DComponent component_to_install) {
+	public static final void installComponent (final Scene2DComponent component_to_install) {
 		componentInstaller.installComponent(component_to_install);
 	}
 
-	public static final Scene2DComponent invoke() {
+	public static final Scene2DComponent invoke () {
 		return componentInstaller.invokeComponent();
 	}
 
-	public static final Scene2DComponent component() {
+	public static final Scene2DComponent component () {
 		return componentInstaller.getComponent();
 	}
 
-	public static Scene2DSpawningConfig newSceneSpawningConfig() {
+	public static Scene2DSpawningConfig newSceneSpawningConfig () {
 		return invoke().newSceneSpawningConfig();
 	}
 
-	public static Scene spawnScene(ComponentsFactory components_factory,
-			Scene2DSpawningConfig config) {
+	public static Scene spawnScene (final ComponentsFactory components_factory, final Scene2DSpawningConfig config) {
 		return invoke().spawnScene(components_factory, config);
 	}
 
-//	public static Scene spawnScene(ComponentsFactory components_factory,
-//			SceneStructure structure) {
-//		return invoke().spawnScene(components_factory, structure);
-//	}
+// public static Scene spawnScene(ComponentsFactory components_factory,
+// SceneStructure structure) {
+// return invoke().spawnScene(components_factory, structure);
+// }
 
-	public static PackageReader getPackageReader() {
+	public static PackageReader getPackageReader () {
 		return invoke().getPackageReader();
 	}
 
-//	public static Scene spawnScene(ComponentsFactory components_factory,
-//			AssetID name) {
-//		return invoke().spawnScene(components_factory, name);
-//	}
+	public static void disposeScene (final Scene loader_scene) {
+		invoke().disposeScene(loader_scene);
+	}
+
+// public static Scene spawnScene(ComponentsFactory components_factory,
+// AssetID name) {
+// return invoke().spawnScene(components_factory, name);
+// }
 
 }
--- api/red-triplane-scene2d-api/red-triplane-scene2d-api/com/jfixby/r3/ext/api/scene2d/Scene2DComponent.java
+++ api/red-triplane-scene2d-api/red-triplane-scene2d-api/com/jfixby/r3/ext/api/scene2d/Scene2DComponent.java
@@ -1,3 +1,4 @@
+
 package com.jfixby.r3.ext.api.scene2d;
 
 import com.jfixby.r3.api.ui.unit.ComponentsFactory;
@@ -5,14 +6,16 @@ import com.jfixby.rana.api.pkg.PackageReader;
 
 public interface Scene2DComponent {
 
-	Scene2DSpawningConfig newSceneSpawningConfig();
+	Scene2DSpawningConfig newSceneSpawningConfig ();
 
-	Scene spawnScene(ComponentsFactory components_factory, Scene2DSpawningConfig config);
+	Scene spawnScene (ComponentsFactory components_factory, Scene2DSpawningConfig config);
 
 	// Scene spawnScene(ComponentsFactory components_factory,
 	// SceneStructure structure);
 
-	public PackageReader getPackageReader();
+	public PackageReader getPackageReader ();
+
+	void disposeScene (Scene loader_scene);
 
 	// Scene spawnScene(ComponentsFactory components_factory, AssetID name);
 
