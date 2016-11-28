9d2af36e69d3f54f56399b875ba7bccaba2a6d7e
J Fixby <github@jfixby.com>
2016-11-28 15:17:49 +0100

    ...

--- api/red-triplane-api/r3-api/com/jfixby/r3/api/EngineParams.java
+++ api/red-triplane-api/r3-api/com/jfixby/r3/api/EngineParams.java
@@ -26,10 +26,4 @@ public class EngineParams {
 		public static final String HomePage = "r3.version.homepage";
 	}
 
-	public static class Application {
-		public static final String Name = "r3.version.name";
-		public static final String BuildID = "r3.version.build_id";
-		public static final String HomePage = "r3.version.homepage";
-	}
-
 }
