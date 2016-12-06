2dd740c728b2e089c0262e20f846a5d82cca77c1
J Fixby <github@jfixby.com>
2016-12-06 13:58:21 +0100

    ...

--- fokker/red-triplane-fokker-assembler/red-triplane-fokker-assembler/com/jfixby/red/triplane/fokker/assembler/PrepareConfig.java
+++ fokker/red-triplane-fokker-assembler/red-triplane-fokker-assembler/com/jfixby/red/triplane/fokker/assembler/PrepareConfig.java
@@ -1,3 +1,4 @@
+
 package com.jfixby.red.triplane.fokker.assembler;
 
 import com.jfixby.cmns.api.file.File;
@@ -10,59 +11,47 @@ import com.jfixby.red.triplane.fokker.assembler.ds.ProjectAssemblerContainer;
 
 public class PrepareConfig {
 
-	public static void main(String[] args) throws Exception {
+	public static void main (final String[] args) throws Exception {
 		DesktopSetup.deploy();
 
-		AssemblerConfig assembler_config = new AssemblerConfig();
+		final AssemblerConfig assembler_config = new AssemblerConfig();
 
-		assembler_config
-				.setGradleOutputFolder("D:\\[DEV]\\[CODE]\\[WS-GDX]\\red-triplane-fokker-1.6.4");
+		assembler_config.setGradleOutputFolder("D:\\[DEV]\\[CODE]\\[WS-GDX]\\red-triplane-fokker-1.6.4");
 
-		File workspace = LocalFileSystem.WorkspaceFolder();
-		assembler_config.setInputWorkspace(workspace.toJavaFile()
-				.getAbsolutePath());
+		final File workspace = null;
 
-		assembler_config
-				.setAssetsInput("D:\\[DEV]\\[GIT-2]\\TintoAssets\\tinto-assets");
+		assembler_config.setAssetsInput("D:\\[DEV]\\[GIT-2]\\TintoAssets\\tinto-assets");
 
 		{
 			addProject("jfixby-commons-api", assembler_config.core);
 			addProject("jfixby-commons-red", assembler_config.core);
 
-			addProject("red-triplane-api", assembler_config.core,
-					"red-triplane-api", "red-triplane-api-ext",
-					"red-triplane-api-refactor");
+			addProject("red-triplane-api", assembler_config.core, "red-triplane-api", "red-triplane-api-ext",
+				"red-triplane-api-refactor");
 			addProject("red-triplane-fokker-api", assembler_config.core);
 
-			addProject("red-triplane-core", assembler_config.core,
-					"red-triplane-core", "red-triplane-ext");
+			addProject("red-triplane-core", assembler_config.core, "red-triplane-core", "red-triplane-ext");
 
-			addProject("red-triplane-fokker-core", assembler_config.core,
-					"red-triplane-fokker-core", "red-triplane-fokker-ext");
+			addProject("red-triplane-fokker-core", assembler_config.core, "red-triplane-fokker-core", "red-triplane-fokker-ext");
 
 			addProject("red-triplane-fokker-frontend", assembler_config.core);
 
-			addProject("red-triplane-fokker-gdx", assembler_config.core,
-					"red-triplane-fokker-gdx", "red-triplane-fokker-gdx-tools",
-					"red-triplane-fokker-gdx-box2d");
+			addProject("red-triplane-fokker-gdx", assembler_config.core, "red-triplane-fokker-gdx", "red-triplane-fokker-gdx-tools",
+				"red-triplane-fokker-gdx-box2d");
 
 			addProject("tinto-ui", assembler_config.core);
 			addProject("tinto-game", assembler_config.core);
 			addProject("tinto-api", assembler_config.core);
 
 			addProject("jfixby-util-filesystem-packing", assembler_config.core);
-			addProject("jfixby-util-filesystem-packing-api",
-					assembler_config.core);
+			addProject("jfixby-util-filesystem-packing-api", assembler_config.core);
 
 			addProject("box2d-api", assembler_config.core);
-			addProject("box2d-red-double", assembler_config.core,
-					"box2d-red-double", "jbox2d-double");
+			addProject("box2d-red-double", assembler_config.core, "box2d-red-double", "jbox2d-double");
 
-			addProject("red-triplane-fokker-backends", assembler_config.core,
-					"red-triplane-fokker-common");
+			addProject("red-triplane-fokker-backends", assembler_config.core, "red-triplane-fokker-common");
 
-			addProject("red-triplane-fokker-core", assembler_config.core,
-					"red-triplane-fokker-core", "red-triplane-fokker-ext");
+			addProject("red-triplane-fokker-core", assembler_config.core, "red-triplane-fokker-core", "red-triplane-fokker-ext");
 
 			addProject("red-triplane-resource-fsbased", assembler_config.core);
 
@@ -70,8 +59,7 @@ public class PrepareConfig {
 
 		}
 		{
-			addProject("red-triplane-fokker-backends",
-					assembler_config.desktop, "red-triplane-fokker-desktop");
+			addProject("red-triplane-fokker-backends", assembler_config.desktop, "red-triplane-fokker-desktop");
 
 			addProject("jfixby-log-desktop", assembler_config.desktop);
 
@@ -85,8 +73,7 @@ public class PrepareConfig {
 
 		}
 		{
-			addProject("red-triplane-fokker-backends",
-					assembler_config.android, "red-triplane-fokker-android");
+			addProject("red-triplane-fokker-backends", assembler_config.android, "red-triplane-fokker-android");
 		}
 		{
 			// addProject("red-triplane-fokker-backends", assembler_config.html,
@@ -94,35 +81,30 @@ public class PrepareConfig {
 
 		}
 		{
-			addProject("red-triplane-fokker-backends", assembler_config.ios,
-					"red-triplane-fokker-ios");
+			addProject("red-triplane-fokker-backends", assembler_config.ios, "red-triplane-fokker-ios");
 		}
 
 		assembler_config.print();
 
-		String config_string = Json.serializeToString(assembler_config).toString();
+		final String config_string = Json.serializeToString(assembler_config).toString();
 
-		String java_path = "fokker-assembler.config";
-		File mp = LocalFileSystem.newFile(java_path);
+		final String java_path = "fokker-assembler.config";
+		final File mp = LocalFileSystem.newFile(java_path);
 		mp.writeString(config_string);
 
 	}
 
-	
-	private static void addProject(String project_name,
-			ProjectAssemblerContainer container) {
-		ProjectAssemblerConfig project = new ProjectAssemblerConfig(
-				project_name);
+	private static void addProject (final String project_name, final ProjectAssemblerContainer container) {
+		final ProjectAssemblerConfig project = new ProjectAssemblerConfig(project_name);
 		project.addSourceFolder();
 		container.addProject(project);
 	}
 
-	private static void addProject(String project_name,
-			ProjectAssemblerContainer container, String... source_folders) {
-		ProjectAssemblerConfig project = new ProjectAssemblerConfig(
-				project_name);
+	private static void addProject (final String project_name, final ProjectAssemblerContainer container,
+		final String... source_folders) {
+		final ProjectAssemblerConfig project = new ProjectAssemblerConfig(project_name);
 		for (int i = 0; i < source_folders.length; i++) {
-			String folder = source_folders[i];
+			final String folder = source_folders[i];
 			project.addSourceFolder(folder);
 		}
 
