af5fc975b68f7d22b1730a6f911cb04b577e5553
J Fixby <github@jfixby.com>
2017-01-27 17:00:34 +0100

    +FILL_SCREEN_APERTURE_WRAP

--- api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/user/MouseInputEventListener.java
+++ api/red-triplane-api/r3-api/com/jfixby/r3/api/ui/unit/user/MouseInputEventListener.java
@@ -1,6 +1,7 @@
 
 package com.jfixby.r3.api.ui.unit.user;
 
+import com.jfixby.r3.api.ui.unit.input.MouseExitEvent;
 import com.jfixby.r3.api.ui.unit.input.MouseMovedEvent;
 import com.jfixby.r3.api.ui.unit.input.TouchDownEvent;
 import com.jfixby.r3.api.ui.unit.input.TouchDraggedEvent;
@@ -8,42 +9,33 @@ import com.jfixby.r3.api.ui.unit.input.TouchUpEvent;
 import com.jfixby.r3.api.ui.unit.layer.Component;
 import com.jfixby.scarabei.api.log.L;
 
-public interface MouseInputEventListener extends Component {
+public abstract class MouseInputEventListener implements Component {
 
-	public static final MouseInputEventListener DEBUG = new MouseInputEventListener() {
+	public static final MouseInputEventListener DEBUG = new MouseInputEventListener() {};
 
-		@Override
-		public boolean onMouseMoved (final MouseMovedEvent event) {
-			L.d("DEBUG", event);
-			return true;
-		}
+	public boolean onMouseMoved (final MouseMovedEvent event) {
+		L.d("" + this, event);
+		return true;
+	}
 
-		@Override
-		public boolean onTouchDown (final TouchDownEvent event) {
-			L.d("DEBUG", event);
-			return true;
-		}
+	public boolean onTouchDown (final TouchDownEvent event) {
+		L.d("" + this, event);
+		return true;
+	}
 
-		@Override
-		public boolean onTouchUp (final TouchUpEvent event) {
-			L.d("DEBUG", event);
-			return true;
-		}
+	public boolean onTouchUp (final TouchUpEvent event) {
+		L.d("" + this, event);
+		return true;
+	}
 
-		@Override
-		public boolean onTouchDragged (final TouchDraggedEvent event) {
-			L.d("DEBUG", event);
-			return true;
-		}
+	public boolean onTouchDragged (final TouchDraggedEvent event) {
+		L.d("" + this, event);
+		return true;
+	}
 
-	};
-
-	boolean onMouseMoved (MouseMovedEvent input_event);
-
-	boolean onTouchDown (TouchDownEvent input_event);
-
-	boolean onTouchUp (TouchUpEvent input_event);
-
-	boolean onTouchDragged (TouchDraggedEvent input_event);
+	public boolean onMouseExit (final MouseExitEvent event) {
+		L.d("" + this, event);
+		return true;
+	}
 
 }
