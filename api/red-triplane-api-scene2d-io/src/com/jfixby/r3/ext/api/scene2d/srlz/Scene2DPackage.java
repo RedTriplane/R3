c8be0ffd19186c823d554422f632a48ded436b48
J Fixby <github@jfixby.com>
2017-03-11 21:11:20 +0100

    ...

--- api/red-triplane-api-scene2d-io/src/com/jfixby/r3/ext/api/scene2d/srlz/Scene2DPackage.java
+++ api/red-triplane-api-scene2d-io/src/com/jfixby/r3/ext/api/scene2d/srlz/Scene2DPackage.java
@@ -3,9 +3,7 @@ package com.jfixby.r3.ext.api.scene2d.srlz;
 
 import java.util.Vector;
 
-import com.jfixby.rana.api.asset.AssetsGroup;
-
-public class Scene2DPackage implements java.io.Serializable, AssetsGroup {
+public class Scene2DPackage implements java.io.Serializable {
 
 	/**
 	 *
@@ -14,11 +12,6 @@ public class Scene2DPackage implements java.io.Serializable, AssetsGroup {
 	public static final String SCENE2D_PACKAGE_FILE_EXTENSION = "r3-scenes";
 	public static final String SCENE2D_PACKAGE_FORMAT = "RedTriplane.Scene2D";
 
-	public Vector<SceneStructure> structures = new Vector<SceneStructure>();
-
-	@Override
-	public void dispose () {
-
-	}
+	public Vector<SceneStructure> structures = new Vector<>();
 
 }
