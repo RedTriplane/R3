2338efcf2d4da1e979bec617d9fd90a52b4fd2ae
J Fixby <github@jfixby.com>
2017-05-07 21:36:39 +0200

    +raw text

--- api/red-triplane-api-ui/src/com/jfixby/r3/api/ui/unit/txt/TextBar.java
+++ api/red-triplane-api-ui/src/com/jfixby/r3/api/ui/unit/txt/TextBar.java
@@ -7,6 +7,10 @@ import com.jfixby.r3.api.ui.unit.locale.LocalizedComponent;
 
 public interface TextBar extends VisibleComponent, LocalizedComponent, CanvasPositionable {
 
+	void setRawText (String string);
+
+	String getRawText ();
+
 // Rectangle shape ();
 
 }
