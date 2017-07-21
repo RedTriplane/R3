ef85f6ab4ed6beab4219cca10ca53ba04815f33c
JFixby <github@jfixby.com>
2017-07-21 14:38:07 +0200

    red

--- r3-activity/src/com/jfixby/r3/io/scene2d/ActionsGroup.java
+++
@@ -1,9 +0,0 @@
-
-package com.jfixby.r3.io.scene2d;
-
-import java.util.ArrayList;
-
-public class ActionsGroup {
-	public String name;
-	public ArrayList<Action> actions = null;
-}
--- r3-activity/src/com/jfixby/r3/io/scene2d/ShaderParameterType.java
+++
@@ -1,6 +0,0 @@
-package com.jfixby.r3.io.scene2d;
-
-public enum ShaderParameterType {
-    FLOAT, INT
-
-}
